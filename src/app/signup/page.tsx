19d7329c363d41ad0236ee12bef7bc4aeaa6b580
Muhammad Hassan <hassandesigns4@gmail.com>
2025-10-07 01:47:37 +0500

    added signup route for user in front-end

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,20 +1,53 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios  from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage() {
+  const router = useRouter();
+
   const [user, setUser] = useState({
     email: "",
     password: "",
     username: "",
   });
 
-  const onSignup = async () => {};
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      toast.success("Signup successful");
+      router.push("/login");
+
+    } catch (error:unknown) {
+     if(error instanceof Error) {
+       console.log("Signup failed", error.message);
+
+       toast.error(error.message);
+     } 
+    }
+    finally{
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen py-2">
-      <h1>Signup</h1>
+      <h1 className="text-4xl mb-2">{loading ? "Processing": "Signup"}</h1>
       <hr />
       <label htmlFor="username">Username</label>
       <input
@@ -50,7 +83,7 @@ export default function SignupPage() {
         onClick={onSignup}
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
       >
-        Signup
+        {buttonDisabled ? "Please fill all the fields to Signup" : "Signup"}
       </button>
 
       <Link href="/login" className="mt-4 ">
